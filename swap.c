7a524c3d27e379fc9f1eef58b6c1b66f11b84b2e
Abelam2001 <85018795+Abelam2001@users.noreply.github.com>
2021-07-01 23:50:38 +0530

    Update swap.c

--- swap.c
+++ swap.c
@@ -1,4 +1,4 @@
-int swap_max(int ar[], int l, int n){
+int *swap_max(int ar[], int l, int n){
 int temp=ar[n];
 int max=0;
 for(int i=n;i<l;++i){
@@ -8,7 +8,7 @@ max=ar[i+1];
 ar[n]=max;
 return ar;
 }
-int ssort(int ar[],int l){
+int *ssort(int ar[],int l){
 for(int i=0;i<l;++i){
 swap_max(ar,l,i);
 }
